eef40bf36a4f2d8c0b76e5adbcbecee32c7d9446
SeoJiWon <sjiwon4491@gmail.com>
2023-03-04 17:32:19 +0900

    feat: Email, Password VO 구현

---
+++ src/main/java/com/kgu/studywithme/member/domain/Email.java
@@ -0,0 +1,42 @@
+package com.kgu.studywithme.member.domain;
+
+import com.kgu.studywithme.global.exception.StudyWithMeException;
+import com.kgu.studywithme.member.exception.MemberErrorCode;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.regex.Pattern;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Email {
+    // 이메일 형식은 @gmail.com만 허용
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@gmail\\.com$";
+    private static final Pattern EMAIL_MATCHER = Pattern.compile(EMAIL_PATTERN);
+
+    @Column(name = "email", nullable = false, unique = true, updatable = false, length = 150)
+    private String value;
+
+    private Email(String value) {
+        this.value = value;
+    }
+
+    public static Email from(String value) {
+        validateEmailPattern(value);
+        return new Email(value);
+    }
+
+    private static void validateEmailPattern(String value) {
+        if (isNotValidPattern(value)) {
+            throw StudyWithMeException.type(MemberErrorCode.INVALID_EMAIL_PATTERN);
+        }
+    }
+
+    private static boolean isNotValidPattern(String email) {
+        return !EMAIL_MATCHER.matcher(email).matches();
+    }
+}
