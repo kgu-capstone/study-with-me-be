58f5c96be9f5c92502d2dbcacbdf33ce6976f87e
sjiwon <sjiwon4491@gmail.com>
2023-03-05 21:46:08 +0900

    [feat] Member Entity 수정과 스터디 카테고리에 대한 설계를 진행한다 (#17)
    * chore: local 환경에 대한 profile active 수정
    
    * feat: 사용자 거주지에 대한 Region VO 구현
    
    * rename: main 네이밍 수정
    
    * feat: 사용자 닉네임에 대한 Nickname VO 구현
    
    * feat: 기본 스터디 카테고리 구현
    
    * feat: 사용자의 관심사 목록에 대한 @ElementCollection 구현

--- src/main/java/com/kgu/studywithme/StudyWithMeApplication.java
+++ src/main/java/com/kgu/studywithme/StudyWithMeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudywithmeApplication {
+public class StudyWithMeApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(StudywithmeApplication.class, args);
+		SpringApplication.run(StudyWithMeApplication.class, args);
 	}
 }
--- src/test/java/com/kgu/studywithme/fixture/MemberFixture.java
+++ src/test/java/com/kgu/studywithme/fixture/MemberFixture.java
@@ -1,9 +1,6 @@
 package com.kgu.studywithme.fixture;
 
-import com.kgu.studywithme.member.domain.Email;
-import com.kgu.studywithme.member.domain.Gender;
-import com.kgu.studywithme.member.domain.Member;
-import com.kgu.studywithme.member.domain.Password;
+import com.kgu.studywithme.member.domain.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -14,25 +11,28 @@ import static com.kgu.studywithme.common.utils.PasswordEncoderUtils.ENCODER;
 @Getter
 @RequiredArgsConstructor
 public enum MemberFixture {
-    SEO_JI_WON("서지원", "sjiwon4491@gmail.com", "abcABC123!@#", LocalDate.of(2000, 1, 18), Gender.MALE, "안양"),
+    SEO_JI_WON("서지원", "서지원", "sjiwon4491@gmail.com", "abcABC123!@#", LocalDate.of(2000, 1, 18), Gender.MALE, "경기도", "안양시"),
     ;
 
     private final String name;
+    private final String nickname;
     private final String email;
     private final String password;
     private final LocalDate birth;
     private final Gender gender;
-    private final String location;
+    private final String province;
+    private final String city;
 
     public Member toMember() {
         return Member.builder()
                 .name(name)
+                .nickname(Nickname.from(nickname))
                 .email(Email.from(email))
                 .password(Password.encrypt(password, ENCODER))
                 .birth(birth)
                 .phone(generateRandomPhoneNumber())
                 .gender(gender)
-                .location(location)
+                .region(Region.of(province, city))
                 .build();
     }
 
