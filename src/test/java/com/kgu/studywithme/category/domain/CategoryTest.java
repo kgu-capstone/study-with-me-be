de96cf27c8ec3adf31feb880d3b223aa1769450b
SeoJiWon <sjiwon4491@gmail.com>
2023-03-06 15:25:11 +0900

    test: 없는 카테고리에 대한 조회 테스트 추가

--- src/test/java/com/kgu/studywithme/category/domain/CategoryTest.java
+++ src/test/java/com/kgu/studywithme/category/domain/CategoryTest.java
@@ -1,17 +1,20 @@
 package com.kgu.studywithme.category.domain;
 
+import com.kgu.studywithme.category.exception.CategoryErrorCode;
+import com.kgu.studywithme.global.exception.StudyWithMeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.kgu.studywithme.category.domain.Category.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("Category 도메인 테스트")
 class CategoryTest {
     @Test
     @DisplayName("특정 카테고리를 조회한다")
-    void test() {
+    void findSpecificCategory() {
         // given
         final long language = 1L;
         final long interview = 2L;
@@ -29,5 +32,17 @@ class CategoryTest {
                 () -> assertThat(Category.from(certification)).isEqualTo(CERTIFICATION),
                 () -> assertThat(Category.from(etc)).isEqualTo(ETC)
         );
+    }
+
+    @Test
+    @DisplayName("없는 카테고리를 조회하면 예외가 발생한다")
+    void findAnonymousCategory() {
+        // given
+        final long anonymous = 100000000L;
+
+        // when - then
+        assertThatThrownBy(() -> Category.from(anonymous))
+                .isInstanceOf(StudyWithMeException.class)
+                .hasMessage(CategoryErrorCode.CATEGORY_NOT_EXIST.getMessage());
     }
 }
