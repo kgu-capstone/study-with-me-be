25dc95f61552e8e3fe8aa212bc18762ca050207b
SeoJiWon <sjiwon4491@gmail.com>
2023-03-07 09:16:24 +0900

    feat: OAuth 요청 스펙 Properties 생성

---
+++ src/main/java/com/kgu/studywithme/auth/infra/oauth/OAuthProperties.java
@@ -0,0 +1,46 @@
+package com.kgu.studywithme.auth.infra.oauth;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Getter
+@Component
+public class OAuthProperties {
+    private final String grantType;
+    private final String clientId;
+    private final String clientSecret;
+    private final String clientAuthenticationMethod;
+    private final String redirectUrl;
+    private final Set<String> scope;
+    private final String authUrl;
+    private final String tokenUrl;
+    private final String userInfoUrl;
+    private final String usernameAttribute;
+
+    public OAuthProperties(
+            @Value("${oauth2.google.grant-type}") String grantType,
+            @Value("${oauth2.google.client-id}") String clientId,
+            @Value("${oauth2.google.client-secret}") String clientSecret,
+            @Value("${oauth2.google.client-authentication-method}") String clientAuthenticationMethod,
+            @Value("${oauth2.google.redirect-url}") String redirectUrl,
+            @Value("${oauth2.google.scope}") Set<String> scope,
+            @Value("${oauth2.google.auth-url}") String authUrl,
+            @Value("${oauth2.google.token-url}") String tokenUrl,
+            @Value("${oauth2.google.user-info-url}") String userInfoUrl,
+            @Value("${oauth2.google.username-attribute}") String usernameAttribute
+    ) {
+        this.grantType = grantType;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.clientAuthenticationMethod = clientAuthenticationMethod;
+        this.redirectUrl = redirectUrl;
+        this.scope = scope;
+        this.authUrl = authUrl;
+        this.tokenUrl = tokenUrl;
+        this.userInfoUrl = userInfoUrl;
+        this.usernameAttribute = usernameAttribute;
+    }
+}
