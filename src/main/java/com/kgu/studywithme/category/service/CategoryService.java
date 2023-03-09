85cf7de91113df70f22ea1228c0ee1b9aa8667fa
SeoJiWon <sjiwon4491@gmail.com>
2023-03-09 20:35:06 +0900

    feat: 전체 카테고리 조회 로직 구현

---
+++ src/main/java/com/kgu/studywithme/category/service/CategoryService.java
@@ -0,0 +1,22 @@
+package com.kgu.studywithme.category.service;
+
+import com.kgu.studywithme.category.domain.Category;
+import com.kgu.studywithme.category.service.dto.response.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CategoryService {
+    public List<CategoryResponse> findAll() {
+        return Arrays.stream(Category.values())
+                .map(CategoryResponse::new)
+                .collect(Collectors.toList());
+    }
+}
