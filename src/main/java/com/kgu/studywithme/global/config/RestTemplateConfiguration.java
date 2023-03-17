2f023ff0667ca4ad8a3d80730d37dc572466b932
sjiwon <sjiwon4491@gmail.com>
2023-03-17 10:50:24 +0900

    refactor: RestTemplate 빈 설정 수정 (#56)

--- src/main/java/com/kgu/studywithme/global/config/RestTemplateConfiguration.java
+++ src/main/java/com/kgu/studywithme/global/config/RestTemplateConfiguration.java
@@ -1,13 +1,19 @@
 package com.kgu.studywithme.global.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfiguration {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setReadTimeout(Duration.ofMillis(5000))
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .build();
     }
 }
