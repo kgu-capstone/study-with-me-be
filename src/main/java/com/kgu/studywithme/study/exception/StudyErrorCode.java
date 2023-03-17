1654503915a047bff3eb91da8e79a93559782859
SeoJiWon <sjiwon4491@gmail.com>
2023-03-17 21:13:11 +0900

    feat: 스터디 이름에 대한 StudyName VO 구현

--- src/main/java/com/kgu/studywithme/study/exception/StudyErrorCode.java
+++ src/main/java/com/kgu/studywithme/study/exception/StudyErrorCode.java
@@ -10,6 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum StudyErrorCode implements ErrorCode {
     // Capacity
     CAPACITY_OUT_OF_RANGE(HttpStatus.BAD_REQUEST, "STUDY_001", "스터디 인원은 2명 이상 10명 이하여야 합니다."),
+
+    // StudyName
+    NAME_IS_BLANK(HttpStatus.BAD_REQUEST, "STUDY_002", "스터디 이름은 공백을 허용하지 않습니다."),
+    NAME_LENGTH_OUT_OF_RANGE(HttpStatus.BAD_REQUEST, "STUDY_003", "스터디 이름은 최대 20자까지 가능합니다."),
     ;
 
     private final HttpStatus status;
