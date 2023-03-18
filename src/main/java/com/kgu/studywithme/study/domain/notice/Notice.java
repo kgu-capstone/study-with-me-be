cf26a27b7dd9649d55bce6a8192c849d95e775dc
SeoJiWon <sjiwon4491@gmail.com>
2023-03-18 21:44:24 +0900

    feat: 스터디 공지사항에 대한 Notice, Comment 구현

---
+++ src/main/java/com/kgu/studywithme/study/domain/notice/Notice.java
@@ -0,0 +1,65 @@
+package com.kgu.studywithme.study.domain.notice;
+
+import com.kgu.studywithme.member.domain.Member;
+import com.kgu.studywithme.study.domain.Study;
+import com.kgu.studywithme.study.domain.notice.comment.Comment;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "notice")
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "study_id", referencedColumnName = "id", nullable = false)
+    private Study study;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "writer_id", referencedColumnName = "id", nullable = false)
+    private Member writer;
+
+    @OneToMany(mappedBy = "notice", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    private Notice(Study study, String title, String content) {
+        this.study = study;
+        this.writer = study.getHost();
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Notice writeNotice(Study study, String title, String content) {
+        return new Notice(study, title, content);
+    }
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void addComment(Member writer, String content) {
+        comments.add(Comment.writeComment(this, writer, content));
+    }
+}
---
+++ src/main/java/com/kgu/studywithme/study/domain/notice/NoticeRepository.java
@@ -0,0 +1,6 @@
+package com.kgu.studywithme.study.domain.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
