94e4b07e61a72923021ff9aa5ed4a41afd7bcf2f
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-03-26 14:47:15 +0900

    refactor: from 메소드 삭제

--- src/main/java/com/kgu/studywithme/study/domain/StudyType.java
+++ src/main/java/com/kgu/studywithme/study/domain/StudyType.java
@@ -1,11 +1,7 @@
 package com.kgu.studywithme.study.domain;
 
-import com.kgu.studywithme.category.exception.CategoryErrorCode;
-import com.kgu.studywithme.global.exception.StudyWithMeException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -15,11 +11,4 @@ public enum StudyType {
     ;
 
     private final String description;
-
-    public static StudyType from(String description) {
-        return Arrays.stream(values())
-                .filter(study -> study.description.equals(description))
-                .findFirst()
-                .orElseThrow(() -> StudyWithMeException.type(CategoryErrorCode.CATEGORY_NOT_EXIST));
-    }
 }
