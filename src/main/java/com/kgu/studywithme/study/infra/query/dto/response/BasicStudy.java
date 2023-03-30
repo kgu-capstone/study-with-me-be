30c9e79b53c2ff6861fc98e9e38bba45e60fdc9b
SeoJiWon <sjiwon4491@gmail.com>
2023-03-30 16:25:40 +0900

    feat: 메인페이지 기본 스터디 응답 스펙

---
+++ src/main/java/com/kgu/studywithme/study/infra/query/dto/response/BasicStudy.java
@@ -0,0 +1,45 @@
+package com.kgu.studywithme.study.infra.query.dto.response;
+
+import com.kgu.studywithme.category.domain.Category;
+import com.kgu.studywithme.study.domain.Description;
+import com.kgu.studywithme.study.domain.RecruitmentStatus;
+import com.kgu.studywithme.study.domain.StudyName;
+import com.kgu.studywithme.study.domain.StudyType;
+import com.kgu.studywithme.study.domain.participant.Capacity;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class BasicStudy {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final String category;
+    private final String type;
+    private final String recruitmentStatus;
+    private final long currentMembers;
+    private final int maxMembers;
+    private final LocalDateTime registerDate;
+    private final long favoriteCount;
+    private final long reviewCount;
+
+    @Builder
+    @QueryProjection
+    public BasicStudy(Long id, StudyName name, Description description, Category category, StudyType type, RecruitmentStatus recruitmentStatus,
+                      long currentMembers, Capacity capacity, LocalDateTime registerDate, long favoriteCount, long reviewCount) {
+        this.id = id;
+        this.name = name.getValue();
+        this.description = description.getValue();
+        this.category = category.getName();
+        this.type = type.getDescription();
+        this.recruitmentStatus = recruitmentStatus.getDescription();
+        this.currentMembers = currentMembers;
+        this.maxMembers = capacity.getValue();
+        this.registerDate = registerDate;
+        this.favoriteCount = favoriteCount;
+        this.reviewCount = reviewCount;
+    }
+}
