e07e6149a05e9652577c7fa701f25dae715454de
SeoJiWon <sjiwon4491@gmail.com>
2023-03-30 16:27:00 +0900

    feat: 페이징 관련 상수 인터페이스

---
+++ src/main/java/com/kgu/studywithme/study/utils/PagingConstants.java
@@ -0,0 +1,8 @@
+package com.kgu.studywithme.study.utils;
+
+public interface PagingConstants {
+    int SLICE_PER_PAGE = 8;
+    String SORT_DATE = "date";
+    String SORT_FAVORITE = "favorite";
+    String SORT_REVIEW = "review";
+}
