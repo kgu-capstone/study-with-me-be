d9376ef45f7413d99f329ff6bd00baadf2338903
SeoJiWon <sjiwon4491@gmail.com>
2023-04-02 16:42:54 +0900

    feat: @PathVariable memberId와 Token payloadId 검증에 대한 AOP 로직 구현

---
+++ src/main/java/com/kgu/studywithme/global/annotation/ValidMemberAspect.java
@@ -0,0 +1,22 @@
+package com.kgu.studywithme.global.annotation;
+
+import com.kgu.studywithme.auth.exception.AuthErrorCode;
+import com.kgu.studywithme.global.exception.StudyWithMeException;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ValidMemberAspect {
+    @Before("@annotation(com.kgu.studywithme.global.annotation.ValidMember) && args(memberId, payloadId)")
+    public void checkPayloadId(Long memberId, Long payloadId) {
+        if (isAnonymousMember(memberId, payloadId)) {
+            throw StudyWithMeException.type(AuthErrorCode.INVALID_PERMISSION);
+        }
+    }
+
+    private boolean isAnonymousMember(Long memberId, Long payloadId) {
+        return !memberId.equals(payloadId);
+    }
+}
---
+++ src/main/java/com/kgu/studywithme/global/config/AopConfiguration.java
@@ -0,0 +1,9 @@
+package com.kgu.studywithme.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfiguration {
+}
