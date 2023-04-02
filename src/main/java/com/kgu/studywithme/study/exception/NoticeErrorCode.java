8592fe5b4b6b59a00d72efe559ba6e274ae7aaf8
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-04-03 02:22:26 +0900

    refactor: NOTICE_NOT_FOUND의 status를 HttpStatus.NOT_FOUND -> HttpStatus.CONFLICT 변경

--- src/main/java/com/kgu/studywithme/study/exception/NoticeErrorCode.java
+++ src/main/java/com/kgu/studywithme/study/exception/NoticeErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum NoticeErrorCode implements ErrorCode {
-    NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTICE_001", "공지사항이 존재하지 않습니다."),
+    NOTICE_NOT_FOUND(HttpStatus.CONFLICT, "NOTICE_001", "공지사항이 존재하지 않습니다."),
     ;
 
     private final HttpStatus status;
