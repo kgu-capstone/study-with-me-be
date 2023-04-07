4d49dfe332f98a146ccba4b64228540503aa06aa
SeoJiWon <sjiwon4491@gmail.com>
2023-04-07 09:05:06 +0900

    test: AuthorizationExtractor 테스트 추가

---
+++ src/test/java/com/kgu/studywithme/auth/utils/AuthorizationExtractorTest.java
@@ -0,0 +1,52 @@
+package com.kgu.studywithme.auth.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+import static com.kgu.studywithme.common.utils.TokenUtils.ACCESS_TOKEN;
+import static com.kgu.studywithme.common.utils.TokenUtils.BEARER_TOKEN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@DisplayName("Auth [Utils] -> AuthorizationExtractor 테스트")
+class AuthorizationExtractorTest {
+    private HttpServletRequest request;
+
+    @BeforeEach
+    void setUp() {
+        request = mock(HttpServletRequest.class);
+    }
+    
+    @Test
+    @DisplayName("HTTP Request Message의 Authorization Header에 토큰이 없다면 Optional 빈 값을 응답한다")
+    void emptyToken() {
+        // given
+        given(request.getHeader(AUTHORIZATION)).willReturn(null);
+        
+        // when
+        Optional<String> token = AuthorizationExtractor.extractToken(request);
+
+        // then
+        assertThat(token).isEmpty();
+    }
+    
+    @Test
+    @DisplayName("HTTP Request Message의 Authorization Header에 토큰이 있다면 Optional로 감싸서 응답한다")
+    void tokenExists() {
+        // given
+        given(request.getHeader(AUTHORIZATION)).willReturn(String.join(" ", BEARER_TOKEN, ACCESS_TOKEN));
+
+        // when
+        Optional<String> token = AuthorizationExtractor.extractToken(request);
+
+        // then
+        assertThat(token).isPresent();
+        assertThat(token.get()).isEqualTo(ACCESS_TOKEN);
+    }
+}
