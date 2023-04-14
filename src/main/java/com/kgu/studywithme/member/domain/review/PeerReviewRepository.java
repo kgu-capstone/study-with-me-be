312c7a0bc7b5299f5ba7e3ab49b32b390aba03be
chaeeerish <keukeu464@gmail.com>
2023-04-14 19:02:12 +0900

    feat: 사용자 PeerReview 조회 API

--- src/main/java/com/kgu/studywithme/member/domain/review/PeerReviewRepository.java
+++ src/main/java/com/kgu/studywithme/member/domain/review/PeerReviewRepository.java
@@ -1,6 +1,14 @@
 package com.kgu.studywithme.member.domain.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PeerReviewRepository extends JpaRepository<PeerReview, Long> {
+    @Query("SELECT r.content" +
+            " FROM PeerReview r" +
+            " WHERE r.reviewee.id = :memberId")
+    List<String> findContentByRevieweeId(@Param("memberId") Long memberId);
 }
---
+++ src/main/java/com/kgu/studywithme/member/service/dto/response/PeerReviewAssembler.java
@@ -0,0 +1,8 @@
+package com.kgu.studywithme.member.service.dto.response;
+
+import java.util.List;
+
+public record PeerReviewAssembler(
+        List<String> reviews
+) {
+}
