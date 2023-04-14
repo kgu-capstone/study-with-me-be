aaf58f3589fd9c276d1c75a766e4e9b90199258a
SeoJiWon <sjiwon4491@gmail.com>
2023-04-14 21:10:47 +0900

    refactor: AttachmentTest WeekFixture 활용

--- src/test/java/com/kgu/studywithme/study/domain/week/attachment/AttachmentTest.java
+++ src/test/java/com/kgu/studywithme/study/domain/week/attachment/AttachmentTest.java
@@ -2,16 +2,13 @@ package com.kgu.studywithme.study.domain.week.attachment;
 
 import com.kgu.studywithme.member.domain.Member;
 import com.kgu.studywithme.study.domain.Study;
-import com.kgu.studywithme.study.domain.week.Period;
 import com.kgu.studywithme.study.domain.week.Week;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import static com.kgu.studywithme.fixture.MemberFixture.JIWON;
 import static com.kgu.studywithme.fixture.StudyFixture.SPRING;
+import static com.kgu.studywithme.fixture.WeekFixture.STUDY_WEEKLY_1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -19,8 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class AttachmentTest {
     private static final Member HOST = JIWON.toMember();
     private static final Study STUDY = SPRING.toOnlineStudy(HOST);
-    private static final Period PERIOD = Period.of(LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(7));
-    private static final Week WEEK = Week.createWeek(STUDY, "Week 1", "지정된 시간까지 줌에 접속해주세요.", 1, PERIOD, List.of());
+    private static final Week WEEK = STUDY_WEEKLY_1.toWeek(STUDY);
 
     @Test
     @DisplayName("Attachment를 생성한다")
