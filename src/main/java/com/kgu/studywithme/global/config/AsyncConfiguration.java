f13c14d9fb9f6a25905ff68211409acf792c3195
SeoJiWon <sjiwon4491@gmail.com>
2023-04-21 15:12:16 +0900

    feat: `@Async` 관련 설정 클래스

---
+++ src/main/java/com/kgu/studywithme/global/config/AsyncConfiguration.java
@@ -0,0 +1,34 @@
+package com.kgu.studywithme.global.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration implements AsyncConfigurer {
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAX_POOL_SIZE = 25;
+    private static final int QUEUE_CAPACITY = 10;
+    private static final String THREAD_NAME_PREFIX = "Asynchronous Mail Sender Thread-";
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return AsyncConfigurer.super.getAsyncUncaughtExceptionHandler();
+    }
+}
