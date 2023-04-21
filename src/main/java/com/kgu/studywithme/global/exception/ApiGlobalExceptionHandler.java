5924189bf14227066001320dc70761381e0809ef
sjiwon <sjiwon4491@gmail.com>
2023-04-21 09:40:01 +0900

    [feat] 서버 API 예외에 대한 Slack API 알림 기능을 구현한다 (#149)
    * chore: Slack API 의존성 추가
    
    * feat: 서버 API 예외에 대한 Slack Logging 구현

--- src/main/java/com/kgu/studywithme/global/exception/ApiGlobalExceptionHandler.java
+++ src/main/java/com/kgu/studywithme/global/exception/ApiGlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package com.kgu.studywithme.global.exception;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kgu.studywithme.auth.infra.oauth.dto.response.OAuthUserResponse;
-import lombok.RequiredArgsConstructor;
+import com.slack.api.Slack;
+import com.slack.api.model.Attachment;
+import com.slack.api.model.Field;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -18,15 +21,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.kgu.studywithme.global.slack.SlackMetadata.*;
+import static com.slack.api.webhook.WebhookPayloads.payload;
+
 @Slf4j
 @RestControllerAdvice
-@RequiredArgsConstructor
 public class ApiGlobalExceptionHandler {
+    private static final Slack SLACK_CLIENT = Slack.getInstance();
+
     private final ObjectMapper objectMapper;
+    private final String slackWebhookUrl;
+
+    public ApiGlobalExceptionHandler(ObjectMapper objectMapper,
+                                     @Value("${slack.webhook.url}") String slackWebhookUrl) {
+        this.objectMapper = objectMapper;
+        this.slackWebhookUrl = slackWebhookUrl;
+    }
 
     @ExceptionHandler(StudyWithMeException.class)
     public ResponseEntity<ErrorResponse> studyWithMeException(StudyWithMeException exception) {
@@ -44,19 +62,6 @@ public class ApiGlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(response);
-    }
-
-    /**
-     * Exception
-     */
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleAnonymousException(Exception e) {
-        ErrorCode code = GlobalErrorCode.INTERNAL_SERVER_ERROR;
-        logging(code, e.getMessage());
-
-        return ResponseEntity
-                .status(code.getStatus())
-                .body(ErrorResponse.from(code));
     }
 
     /**
@@ -122,11 +127,27 @@ public class ApiGlobalExceptionHandler {
     }
 
     /**
-     * 내부 서버 오류 전용 ExceptionHandler
+     * 내부 서버 오류 전용 ExceptionHandler (With Slack Logging)
      */
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleAnyException() {
+    public ResponseEntity<ErrorResponse> handleAnyException(Exception e, HttpServletRequest request) {
+        sendSlackAlertErrorLog(e, request);
         return convert(GlobalErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Exception (With Slack Logging)
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAnonymousException(Exception e, HttpServletRequest request) {
+        sendSlackAlertErrorLog(e, request);
+
+        ErrorCode code = GlobalErrorCode.INTERNAL_SERVER_ERROR;
+        logging(code, e.getMessage());
+
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(ErrorResponse.from(code));
     }
 
     private ResponseEntity<ErrorResponse> convert(ErrorCode code) {
@@ -149,5 +170,44 @@ public class ApiGlobalExceptionHandler {
 
     private void logging(ErrorCode code, String message) {
         log.warn("{} | {} | {}", code.getStatus(), code.getErrorCode(), message);
+    }
+
+    public void sendSlackAlertErrorLog(Exception e, HttpServletRequest request) {
+        try {
+            SLACK_CLIENT.send(
+                    slackWebhookUrl,
+                    payload(p -> p
+                            .text("서버 에러 발생!!")
+                            .attachments(
+                                    List.of(generateSlackErrorAttachments(e, request))
+                            )
+                    ));
+        } catch (IOException slackApiError) {
+            log.error("Slack API 통신 간 에러 발생");
+        }
+    }
+
+    private Attachment generateSlackErrorAttachments(Exception e, HttpServletRequest request) {
+        String requestTime = DateTimeFormatter.ofPattern(DATETIME_FORMAT).format(LocalDateTime.now());
+        String xffHeader = request.getHeader(XFF_HEADER);
+        return Attachment.builder()
+                .color(LOG_COLOR)
+                .title(requestTime + " 발생 에러 로그")
+                .fields(
+                        List.of(
+                                generateSlackField(TITLE_REQUEST_IP, (xffHeader == null) ? request.getRemoteAddr() : xffHeader),
+                                generateSlackField(TITLE_REQUEST_URL, request.getMethod() + " " + request.getRequestURL()),
+                                generateSlackField(TITLE_ERROR_MESSAGE, e.getMessage())
+                        )
+                )
+                .build();
+    }
+
+    private Field generateSlackField(String title, String value) {
+        return Field.builder()
+                .title(title)
+                .value(value)
+                .valueShortEnough(false)
+                .build();
     }
 }
