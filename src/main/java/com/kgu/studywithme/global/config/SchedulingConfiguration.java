ee5fbce4683fb7d96f9dae436c358794b3b835a4
SeoJiWon <sjiwon4491@gmail.com>
2023-04-29 12:34:18 +0900

    rename: SchedulerConfiguration -> SchedulingConfiguration

--- src/main/java/com/kgu/studywithme/global/config/SchedulingConfiguration.java
+++ src/main/java/com/kgu/studywithme/global/config/SchedulingConfiguration.java
@@ -5,5 +5,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
-public class SchedulerConfiguration {
+public class SchedulingConfiguration {
 }
