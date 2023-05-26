6df2a3d845c48b9f8f24453bd8d99fe40b9104d5
SeoJiWon <sjiwon4491@gmail.com>
2023-05-26 09:44:33 +0900

    feat: 스터디 생성 시 내부 이미지 업로드에 대한 로직

--- src/main/java/com/kgu/studywithme/upload/utils/BucketMetadata.java
+++ src/main/java/com/kgu/studywithme/upload/utils/BucketMetadata.java
@@ -2,17 +2,22 @@ package com.kgu.studywithme.upload.utils;
 
 public interface BucketMetadata {
     /**
-     * Weekly 글 내부 이미지 업로드 버킷 -> images/{UUID}-{upload file name}
+     * 스터디 생성 시 설명 내부 이미지 업로드 버킷 -> descriptions/{UUID}.{File Extension}
+     */
+    String STUDY_DESCRIPTIONS = "descriptions/%s";
+
+    /**
+     * Weekly 글 내부 이미지 업로드 버킷 -> images/{UUID}.{File Extension}
      */
     String WEEKLY_IMAGES = "images/%s";
 
     /**
-     * Weekly 글 첨부파일 업로드 버킷 -> attachments/{UUID}-{upload file name}
+     * Weekly 글 첨부파일 업로드 버킷 -> attachments/{UUID}.{File Extension}
      */
     String WEEKLY_ATTACHMENTS = "attachments/%s";
 
     /**
-     * Weekly 과제 제출 버킷 -> submits/{UUID}-{upload file name}
+     * Weekly 과제 제출 버킷 -> submits/{UUID}.{File Extension}
      */
     String WEEKLY_SUBMITS = "submits/%s";
 }
