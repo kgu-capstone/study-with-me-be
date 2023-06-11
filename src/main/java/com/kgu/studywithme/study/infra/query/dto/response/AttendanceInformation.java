9dbe7fb2d7d17bac34864747721f79a83b88bb67
SeoJiWon <sjiwon4491@gmail.com>
2023-06-11 20:28:07 +0900

    refactor: 출석 정보 응답 관련 StudyAttendanceMember -> StudyMember 수정

--- src/main/java/com/kgu/studywithme/study/infra/query/dto/response/AttendanceInformation.java
+++ src/main/java/com/kgu/studywithme/study/infra/query/dto/response/AttendanceInformation.java
@@ -2,8 +2,7 @@ package com.kgu.studywithme.study.infra.query.dto.response;
 
 import com.kgu.studywithme.member.domain.Nickname;
 import com.kgu.studywithme.study.domain.attendance.AttendanceStatus;
-import com.kgu.studywithme.study.domain.participant.ParticipantStatus;
-import com.kgu.studywithme.study.service.dto.response.StudyAttendanceMember;
+import com.kgu.studywithme.study.service.dto.response.StudyMember;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,20 +10,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class AttendanceInformation {
-    private final StudyAttendanceMember participant;
+    private final StudyMember participant;
     private final int week;
     private final String attendanceStatus;
 
     @QueryProjection
-    public AttendanceInformation(
-            Long id, Nickname nickname, ParticipantStatus participantStatus,
-            int week, AttendanceStatus attendanceStatus
-    ) {
-        this.participant = new StudyAttendanceMember(
-                id,
-                nickname.getValue(),
-                participantStatus
-        );
+    public AttendanceInformation(Long id, Nickname nickname, int week, AttendanceStatus attendanceStatus) {
+        this.participant = new StudyMember(id, nickname.getValue());
         this.week = week;
         this.attendanceStatus = attendanceStatus.getDescription();
     }
